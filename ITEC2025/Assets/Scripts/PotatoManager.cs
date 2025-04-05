b938d2b33e5f3c0725644bdf01303fd33733d007
JohnnyChris <nistor.ionut66@gmail.com>
2025-04-05 21:26:17 +0300

    every potato works

--- ITEC2025/Assets/Scripts/PotatoManager.cs
+++ ITEC2025/Assets/Scripts/PotatoManager.cs
@@ -1,8 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
-public class PotatoManager : MonoBehaviour
+/*public class PotatoManager : MonoBehaviour
 {
     public GameObject[] objects; // Assign your 3 objects in the Inspector
 
@@ -62,5 +63,69 @@ public class PotatoManager : MonoBehaviour
         {
             objects[i].SetActive(i == index);
         }
+    }
+}
+*/
+public class PotatoManager : MonoBehaviour
+{
+    public GameObject spawnPoint;
+    public GameObject[] objects; // Assign your 3 objects in the Inspector
+    private GameObject currentShape;
+    private int currentIndex = 0;
+
+    void Start()
+    {
+        currentShape = Instantiate(objects[currentIndex], spawnPoint.transform.position, Quaternion.identity);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            SwitchTo(0);
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            SwitchTo(1);
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            SwitchTo(2);
+        }
+    }
+
+    void SwitchTo(int newIndex)
+    {
+        Vector2 currentVelocity; 
+        Vector2 lastPosition;
+        if (currentIndex == 1)
+        {
+            GameObject bone = GameObject.FindGameObjectWithTag("spawnBone");
+            lastPosition = bone.transform.position;
+            currentVelocity = bone.GetComponent<Rigidbody2D>().velocity;
+
+        }
+        else
+        {
+            currentVelocity = currentShape.GetComponent<Rigidbody2D>().velocity;
+            lastPosition = currentShape.transform.position;
+        }
+
+        Destroy(currentShape);
+        currentIndex = newIndex;
+        objects[currentIndex].transform.position = lastPosition;
+        currentShape = Instantiate(objects[currentIndex]);
+        if (newIndex == 1)
+        {
+            List<Rigidbody2D> bones = currentShape.GetComponentsInChildren<Rigidbody2D>().ToList();
+            foreach (Rigidbody2D bone in bones)
+            {
+                bone.velocity = currentVelocity;
+            }
+
+        }
+            else {
+                currentShape.GetComponent<Rigidbody2D>().velocity = currentVelocity;
+            }
     }
 }
