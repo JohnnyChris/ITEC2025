862905b725baaacee1cccffc7cad1f8d7775e83b
JohnnyChris <nistor.ionut66@gmail.com>
2025-04-06 06:21:05 +0300

    added stuff

---
+++ ITEC2025/Assets/Scripts/MusicManager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour
+{
+    public static MusicManager instance;
+
+    private void Awake()
+    {
+        // Singleton pattern: keep only one instance
+        if (instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        else
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+}
