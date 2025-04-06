efc1db3a84ed5e03df22741d77f6584aa184a434
JohnnyChris <nistor.ionut66@gmail.com>
2025-04-06 12:01:58 +0300

    finaladevarat

--- ITEC2025/Assets/Scripts/FriesDespawm.cs
+++ ITEC2025/Assets/Scripts/FriesDespawm.cs
@@ -12,7 +12,16 @@ public class FriesDespawm : MonoBehaviour
 
     private IEnumerator DestroyBullet()
     {
-        yield return new WaitForSeconds(5f);
+        yield return new WaitForSeconds(4f);
+
+        for (int i = 0; i < 5; i++)
+        {
+            yield return new WaitForSeconds(0.2f);
+            gameObject.GetComponent<SpriteRenderer>().color = new Color(1.0f, 1.0f, 1.0f, 0.0f); ;
+            yield return new WaitForSeconds(0.2f);
+            gameObject.GetComponent<SpriteRenderer>().color = new Color(1.0f, 1.0f, 1.0f, 1.0f); ;
+        }
+        
         Destroy(gameObject);
     }
 }
